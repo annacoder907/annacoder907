3f5981ea1c957a3ed1c24ad73fba813af9d0fd7c
annacoder907 <b8802065@gmail.com>
2025-02-11 05:46:51 -0500

    Add files via upload

---
+++ VS code/demo1.py
@@ -0,0 +1,9 @@
+
+a = 0
+if a==1:
+    print(a+1)
+
+print(a+1)
+
+
+
---
+++ VS code/machine learning.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 50]
+
+plt.plot(x, y)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Test Plot")
+plt.show()  # Use plt.show() in normal Python scripts
+
